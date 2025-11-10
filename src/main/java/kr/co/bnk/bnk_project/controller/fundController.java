8c9a283fcda22336d4eab5f27e00a5ef2642f4f0
dream1910 <dream1910@naver.com>
2025-11-10 11:05:49 +0900

    Controller 추가

--- src/main/java/kr/co/bnk/bnk_project/controller/fundController.java
+++ src/main/java/kr/co/bnk/bnk_project/controller/fundController.java
@@ -8,6 +8,51 @@ public class fundController {
 
     @GetMapping("/fund/depositGuide")
     public String depositGuide() {
-        return "fund/depositGuide"; // templates/fund/depositGuide.html
+        return "fund/depositGuide";
     }
+
+    @GetMapping("/FAQ")
+    public String faq() {
+        return "faq";
+    }
+
+    @GetMapping("/productList")
+    public String productList() {
+        return "productList";
+    }
+    @GetMapping("/productDetail")
+    public String productDetail() {
+        return "productDetail";
+    }
+
+    @GetMapping("/investorInfo")
+    public String investorInfo() {
+        return "investorInfo";
+    }
+
+    @GetMapping("/fundSusi")
+    public String fundSusi() {
+        return "fundSusi";
+    }
+
+    @GetMapping("/fundSihwang")
+    public String fundSihwang() {
+        return "fundSihwang";
+    }
+
+    @GetMapping("/fundInformation")
+    public String fundInfromation() {
+        return "fundInformation";
+    }
+
+    @GetMapping("/fundGuide")
+    public String fundGuide() {
+        return "fundGuide";
+    }
+
+    @GetMapping("/dopisitGuide")
+    public String dopisitGuide() {
+        return "dopisitGuide";
+    }
+
 }
